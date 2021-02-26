9c476fe29c53717d62513a1900dd4d05537ecc5c
amrutsabale <samrut355@gmail.com>
2021-02-26 22:03:10 +0530

    Implemented Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
-import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div >
+    <>
+      <Particles
+        params={{
+          particles: {
+            number:
+              { value: 30, density: { enable: true, value_area: 900 } },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+
+            }
+          }
+        }} />
       <Navbar />
-    </div>
+      <Header />
+    </>
   );
 }
 
