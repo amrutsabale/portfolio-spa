31041cb6b9c10d6d9f76f336afe9118d742c60dd
amrutsabale <samrut355@gmail.com>
2021-04-02 11:36:34 +0530

    Updated Resume & Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -13,17 +13,16 @@ const Experience = () => {
             <h3>2019-Present</h3>
             <p>
               I am ReactJs Developer in Cognizant Technology Solutions.
-              Developed and modernized 3 production ready projects for Financial
-              Services clients using ReactJs. Collaborated with team to design &
-              develop reusable components with storybook to be used with the
-              organization’s new products. During projects worked on building
-              new components, ContextAPI & Redux patterns for state management,
-              React Bootstrap for responsive UI design, Jest, Enzyme and React
-              Testing library for writing unit tests in ReactJS. Familiar with
-              Jenkins tool for CI/CD pipline deployment and SonarQube for Code
-              Quality.Performed Automated accessibility testing using
-              cypress-axe in MVPs.Written full fledged end to end testcases
-              using Cypress.
+              Developed and modernized 3 production ready projects for clients
+              using ReactJs. Collaborated with team to design & develop reusable
+              components with storybook to be used with the organization’s new
+              products. During projects worked on building new components,
+              ContextAPI & Redux patterns for state management, React Bootstrap
+              for responsive UI design, Jest, Enzyme and React Testing library
+              for writing unit tests in ReactJS. Familiar with Jenkins tool for
+              CI/CD pipline deployment and SonarQube for Code Quality.Performed
+              Automated accessibility testing using cypress-axe in MVPs.Written
+              full fledged end to end testcases using Cypress.
             </p>
           </div>
         </div>
